b5dabee17fe396ec53ba8baf1897d9e2cede5d7f
Wolaci <wolacijose8@gmail.com>
2019-05-02 10:43:17 -0300

    Criação do index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+    include 'init.php';
+    // setcookie('data', '2019-03-26');
+    if (is_logged()) {
+        header('location: livros.php');
+    }
+    $inputs = [
+        'nome' => 'text',
+        'username' => 'text',
+        'email' => 'text',
+        'senha' => 'password',
+        'senha2' => 'password',
+        'telefone' => 'text',
+        'estado'=>'text',
+        'pais'=>'text'
+    ];
+    /* Um usuário pode se cadastrar ou logar no sistema.
+- Para se cadastrar, devem ser salvos do usuários os seguintes dados:
+- nome
+- nome de usuário
+- email
+- telefone
+- estado de origem
+- país de origem*/
+    $usuariosFile = file('users.csv');
+    $usuarios = [];
+    foreach ($usuariosFile as $usuario) {
+        $usuarioData = explode(',', $usuario);
+        $nome = $usuarioData[0];
+        $usuarios[] = $nome; // adiciona o elemento $nome ao final do array $usuarios
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= TITLE ?> -  Registro de Usuário</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php if (get('message') !== false): ?>
+        <div class="message">
+            <?= get('message') ?>
+        </div>
+    <?php endif ?>
+    <form action="register.php" method="POST">
+        <?php foreach ($inputs as $name => $type): ?>
+            <input type="<?= $type ?>" name="<?= $name ?>" placeholder="<?= $name ?>">
+        <?php endforeach ?>
+        <input type="submit" value="Enviar">
+    </form>
+    <div class="users">
+        <a href="login.php"><h1>Produtos dos usuários</h1></a>
+    </div>
+
+            <?php foreach ($usuarios as $nome): ?>
+                <li><a href="login.php?usuario=<?= $nome ?>"><?= $nome ?></a></li>
+            <?php endforeach ?>
+</body>
+</html>
